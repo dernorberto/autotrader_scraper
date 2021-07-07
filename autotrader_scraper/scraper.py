fd48b82feefd7ad811498499dd9a7f0102380ed8
Norberto Soares <norberto.soares@gmail.com>
2021-07-07 01:32:11 +0100

    Update scraper.py

--- autotrader_scraper/scraper.py
+++ autotrader_scraper/scraper.py
@@ -28,7 +28,7 @@ def get_cars(make="BMW", model="5 SERIES", postcode="SW1A 0AA", radius=1500, min
 	keywords["year"] = [" reg)"]
 	keywords["engine"] = ["engine"]
 	keywords["title"] = ["title"]
-	keywords["distance"] = ["distance"]
+	keywords["distance"] = ["miles"]
 
 	# Set up parameters for query to autotrader.co.uk
 
@@ -110,7 +110,9 @@ def get_cars(make="BMW", model="5 SERIES", postcode="SW1A 0AA", radius=1500, min
 							car["price"] = article.find("div", {"class": "product-card-pricing__price"}).text.strip()
 							##car["distance"] = article.find("li", {"class": "product-card-seller-info__spec-item atc-type-picanto"}).text.split("(")[1].replace(")","")
 							##car["distance"] = article.find("li", {"class": "product-card-seller-info__spec-item atc-type-picanto"}).text.split("miles)")[0][-4:].split("(")[1].strip()
-							car["distance"] = article.find("li", {"class": "product-card-seller-info__spec-item atc-type-picanto"}).text
+							##car["distance"] = article.find("li", {"class": "product-card-seller-info__spec-item atc-type-picanto"}).text
+							car["distance"] = article.find("ul", {"class": "product-card-seller-info__specs"}).text.split("miles)")[0][-4:].split("(")[1].strip()
+
 
 							#distance_list = article.find("ul", {"class": "product-card-seller-info__specs"}).find_all("li")
 							
