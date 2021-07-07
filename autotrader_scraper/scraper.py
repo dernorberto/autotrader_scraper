ffc3613cd751392d3d7e85dfeef9562e2d5c818a
Norberto Soares <norberto.soares@gmail.com>
2021-07-07 01:27:42 +0100

    Update scraper.py

--- autotrader_scraper/scraper.py
+++ autotrader_scraper/scraper.py
@@ -110,14 +110,14 @@ def get_cars(make="BMW", model="5 SERIES", postcode="SW1A 0AA", radius=1500, min
 							car["price"] = article.find("div", {"class": "product-card-pricing__price"}).text.strip()
 							##car["distance"] = article.find("li", {"class": "product-card-seller-info__spec-item atc-type-picanto"}).text.split("(")[1].replace(")","")
 							##car["distance"] = article.find("li", {"class": "product-card-seller-info__spec-item atc-type-picanto"}).text.split("miles)")[0][-4:].split("(")[1].strip()
-							##car["distance"] = article.find("li", {"class": "product-card-seller-info__spec-item atc-type-picanto"}).text
+							car["distance"] = article.find("li", {"class": "product-card-seller-info__spec-item atc-type-picanto"}).text
 
-							distance_list = article.find("ul", {"class": "product-card-seller-info__specs"}).find_all("li")
+							#distance_list = article.find("ul", {"class": "product-card-seller-info__specs"}).find_all("li")
 							
-							for distance_li in distance_list:
-								distance_item = distance_li.text
-								if any(keyword in distance_item for keyword in keywords["distance"]):
-									car["distance"] = int(distance_item[:distance_item.find("miles")].replace(",",""))
+							#for distance_li in distance_list:
+						#		distance_item = distance_li.text
+					#			if any(keyword in distance_item for keyword in keywords["distance"]):
+				#					car["distance"] = int(distance_item[:distance_item.find("miles")].replace(",",""))
 
 
 							key_specs_bs_list = article.find("ul", {"class": "listing-key-specs"}).find_all("li")
